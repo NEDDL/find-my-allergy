169da8d2d7c7300fc2151b1dbf6e10e8e9c29ec7
Mustafa ONAL <mustafasaitonal@outlook.com>
2022-04-25 20:25:51 +0200

    coupling reduced

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 // import "../styles/globals.css";
 import React, { Fragment } from "react";
 import { useRouter } from "next/router";
-import { Toaster } from "react-hot-toast";
 
 import { Provider } from "react-redux";
 import configureStore from "../src/store/configureStore";
 const store = configureStore();
 
 import GlobalStyle from "../styles/globalStyles";
-import { blueGrey, grey, green, orange, red } from "@mui/material/colors";
 import { createTheme, ThemeProvider } from "@mui/material";
 import lightTheme from "../styles/theme/lightTheme";
 import baseOptions from "../styles/theme/baseOptions";
+
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => page);
